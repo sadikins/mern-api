03b21ec3d85c722f00a55f366fab60d89733d67c
sadikins <msadikin1989@gmail.com>
2022-06-08 16:51:52 +0700

    "update controller and routes"

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express= require('express');
 
 const app = express()
 
-app.use(()=> {
-    console.log('hello')
-    console.log('hello dong')
-})
+const productRoutes = require('./src/routes/products');
+
+// All
+// app.use(productRoutes);
+
+// Detail
+app.use('/v1/customer/', productRoutes);
+
 
 app.listen(4000)
 
